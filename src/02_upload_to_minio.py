d51e4b63c2e7c36aacd84e35cc6d9bb28a2142f6
xsorouz <xavier.rousseau5@gmail.com>
2025-05-07 14:46:13 +0200

    Update 02_upload_to_minio.py

--- src/02_upload_to_minio.py
+++ src/02_upload_to_minio.py
@@ -26,7 +26,7 @@ logger.add(LOGS_PATH / "upload_minio.log", level="INFO", rotation="500 KB")
 # ==============================================================================
 # Paramètres MinIO
 # ==============================================================================
-MINIO_ENDPOINT = "http://host.docker.internal:9000"
+MINIO_ENDPOINT = "http://host.docker.internal:9000" # "http://minio:9000" 
 ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
 SECRET_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 BUCKET_NAME = "bottleneck"
